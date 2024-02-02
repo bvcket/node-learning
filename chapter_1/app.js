1a33691afefe895f9311197b872d65ee5ff0d250
bvcket <amanpauljoshua@gmail.com>
2024-02-02 15:57:43 +0800

    chapter 1 commit

---
+++ chapter_1/app.js
@@ -0,0 +1,12 @@
+// let time = 0;
+
+// const timer = setInterval(() => {
+//     time += 1;
+//     console.log(`${time} seconds have passed`);
+//     if (time > 5){
+//         clearInterval(timer);
+//     }
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
